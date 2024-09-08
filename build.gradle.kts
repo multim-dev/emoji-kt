a58d66e02a55a98981c0993f22ef0492ca488a64
usbharu <64310155+usbharu@users.noreply.github.com>
2024-09-08 18:33:32 +0900

    release 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "dev.usbharu"
-version = "2.0.0"
+version = "2.0.1"
 
 repositories {
     google()
--- buildSrc/src/main/kotlin/EmojiPlugin.kt
+++ buildSrc/src/main/kotlin/EmojiPlugin.kt
@@ -1,9 +1,3 @@
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import kotlinx.coroutines.runBlocking
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
